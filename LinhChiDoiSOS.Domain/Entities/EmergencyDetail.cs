9a54fc3c855b16eccb31c34204c78557d722c97c
vinnt060402 <vinnt060402@gmail.com>
2024-01-23 00:41:06 +0700

    Update Entity

--- LinhChiDoiSOS.Domain/Entities/EmergencyDetail.cs
+++ LinhChiDoiSOS.Domain/Entities/EmergencyDetail.cs
@@ -5,23 +5,17 @@ public class EmergencyDetail : BaseAuditableEntity
     public int Step { get; set; }
     public string? TopContent { get; set; }
     public string? ContentLeft { get; set; }
+    public string? ContentLeftUrl { get; set; }
     public string? ContentRight { get; set; }
+    public string? ContentRightUrl { get; set; }    
     public string? MainPictureUrl { get; set; }
     public string? ActionContent { get; set; }
     public string? ContentBottom { get; set; }
-    public List<MiniContent>? ListMiniContent { get; set; }
-    public List<AnswerContent>? ListAnswerContent { get; set; }
+
     [ForeignKey("Emergency")]
     public Guid? EmergencyId { get; set; }
 
     public virtual Emergency Emergency { get; set; }
-}
-
-public class MiniContent : BaseAuditableEntity
-{
-    public string? Content { get; set; }
-}
-public class AnswerContent : BaseAuditableEntity
-{
-    public string? Answer { get; set; }
+    public List<MiniContent>? ListMiniContent { get; set; }
+    public List<AnswerContent>? ListAnswerContent { get; set; }
 }
